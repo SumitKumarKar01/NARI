290d2257308e87714814227a2d9c0269867bf3e1
SumitKar01 <sumit.kar@northsouth.edu>
2023-11-13 14:07:07 +0600

    Gradle Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.nari.app"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
