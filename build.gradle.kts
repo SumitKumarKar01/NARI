ef38d3b2f217b6c47985c53e1878fb4c0c2d5ca1
SumitKar01 <sumit.kar@northsouth.edu>
2024-06-18 01:01:18 +0600

    Updated Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 buildscript {
     val kotlin_version by extra("1.9.22")
     dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
     }
     repositories {
@@ -12,7 +11,6 @@ buildscript {
 plugins {
     id("com.android.application") version "8.4.1" apply false
     id("com.android.library") version "8.4.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
